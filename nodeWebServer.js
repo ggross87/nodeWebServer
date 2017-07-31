fcca77debbf94d17d141258e70504284df38065c
Germanual Gross <germanualgross@Germanuals-MacBook-Air.local>
2017-07-31 09:54:36 -0500

    new

---
+++ nodeWebServer.js
@@ -0,0 +1,29 @@
+//include http module
+var http = require('http');
+//declare port
+var GOODPORT = 7000;
+var BADPORT = 7500;
+var status;
+
+//function to handle
+function handleRequests(request, response){
+  response.end(status + request.url);
+}
+
+//function to handle
+function handleBadRequests(request, response){
+  response.end("You're awful and everyone hates you!" + request.url);
+}
+
+// server
+var server = http.createServer(handleRequests);
+var badserver = http.createServer(handleBadRequests);
+
+//LISTEN ON PORT 7000
+server.listen(GOODPORT, function(){
+  console.log('SERVER LISTENING ON: http://localhost:%s', GOODPORT);
+});
+
+badserver.listen(BADPORT, function(){
+  console.log('SERVER LISTENING ON: http://localhost:%s', BADPORT);
+});
